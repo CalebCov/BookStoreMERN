95fb7e5ae286e1da8344edae08c42fd93f8a3307
CalebCov <calebinfour@gmail.com>
2023-12-12 15:57:54 -0600

    Added Get By Id, Get All, and Put Route

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,66 @@ app.post('/books', async (request, response) => {
         console.log(error.message);
         response.status(500).send({ message: error.message });
     }
-})
+});
 
+// Route to Get All Books from the database
+app.get('/books', async (request, response) => {
+    try {
+        const books = await Book.find({});
 
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+// Route to Get One Book from the database by ID
+app.get('/books/:id', async (request, response) => {
+    try {
+
+        const { id } = request.params;
+        
+        const books = await Book.findById(id);
+
+        return response.status(200).json(books);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
+
+// Route to Update a Book 
+app.put('/books/:id', async (request, response) => {
+    try {
+
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title, author, publishYear',
+            });
+        }
+
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+        return response.status(200).json({ message: 'Book updated!' });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
