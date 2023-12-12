028c943b43e10829638bd59542fcd5d84891b149
CalebCov <calebinfour@gmail.com>
2023-12-12 17:25:44 -0600

    Added Delete Route

--- backend/index.js
+++ backend/index.js
@@ -100,6 +100,26 @@ app.put('/books/:id', async (request, response) => {
     }
 });
 
+// Route for Deleting a Book
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+
+        const result = await Book.findByIdAndDelete(id);
+
+        if (!result) {
+            return response.status(404).json({ message: 'Book not found' });
+        }
+
+        return response.status(200).json({ message: 'Book removed!' });
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});   
+    }
+});
+
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
