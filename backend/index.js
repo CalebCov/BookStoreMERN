c226fb59f5620cb49bfc1623431d4baa72cda14b
CalebCov <calebinfour@gmail.com>
2023-12-12 20:27:12 -0600

    Book List with Show, Edit, and Delete React

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,16 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS Policy Cross-Origin Resource Sharing
+//Allow all origins with default of cors(*)
+app.use(cors());
 //Allow Custom Origins
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 app.get('/', (request, response) => {
     console.log(request)
