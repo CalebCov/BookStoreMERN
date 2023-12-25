5d1d20da998f2f64eddf8eaedead9a535688c856
CalebCov <calebinfour@gmail.com>
2023-12-25 16:00:34 -0600

    random comments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,9 +6,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
 
+// Create a root for ReactDOM to render the application
+// Using createRoot for Concurrent Mode (experimental feature as of React 18)
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // Wrap the entire application with BrowserRouter for client-side routing
   <BrowserRouter>
+    {/* Use SnackbarProvider to manage snackbars/toasts for notifications */}
     <SnackbarProvider>
+      {/* Render the main App component */}
       <App />
     </SnackbarProvider>
   </BrowserRouter>
